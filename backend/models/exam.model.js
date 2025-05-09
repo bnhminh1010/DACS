5a77ea592832143799a003c8ff43b41c9a75e37e
MinhTuyetVoi <pata10102004@gmail.com>
2025-05-09 14:45:33 +0700

    Update_7_Docs

--- backend/models/exam.model.js
+++ backend/models/exam.model.js
@@ -1,4 +1,5 @@
 const { Pool } = require("pg");
+// Tạo một pool đăng nhập vào db ( pool giúp tái sử dụng kết nối, tối ưu hiệu năng khi có nhiều request đồng thời)
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -8,6 +9,7 @@ const pool = new Pool({
 });
 
 const Exam = {
+  // Tạo bài kiểm tra
   async create(title, teacherId) {
     const query = {
       text: "INSERT INTO exams(title, teacher_id, created_at, updated_at) VALUES($1, $2, NOW(), NOW()) RETURNING *",
@@ -16,7 +18,7 @@ const Exam = {
     const result = await pool.query(query);
     return result.rows[0];
   },
-
+  // Lấy bài kiểm tra theo id giáo viên
   async findByTeacherId(teacherId) {
     const query = {
       text: "SELECT * FROM exams WHERE teacher_id = $1",
@@ -26,6 +28,7 @@ const Exam = {
     return result.rows;
   },
 
+  // Lây bài kiểm tra bằng id exam
   async findById(examId) {
     const query = {
       text: "SELECT * FROM exams WHERE id = $1",
@@ -35,6 +38,7 @@ const Exam = {
     return result.rows[0];
   },
 
+  // Trạng thái bài kiểm tra được public
   async updatePublishStatus(examId, isPublished) {
     const query = {
       text: "UPDATE exams SET is_published = $1, updated_at = NOW() WHERE id = $2 RETURNING *",
@@ -44,6 +48,7 @@ const Exam = {
     return result.rows[0];
   },
 
+  // Lấy bái kiểm tra được public
   async getPublishedExams() {
     const result = await pool.query(
       "SELECT * FROM exams WHERE is_published = true"
